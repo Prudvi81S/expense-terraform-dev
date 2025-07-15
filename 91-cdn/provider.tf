e55ca0dc41429e93bc3114fb1829e2cc2230d1b7
prudviraj <prudvirajvemu316@gmail.com>
2025-07-15 19:51:31 +0530

    terraform

--- 91-cdn/provider.tf
+++ 91-cdn/provider.tf
@@ -2,16 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.66.0"
+      version = "6.0.0"
     }
   }
 
   backend "s3" {
     bucket         = "81p-remote-state"
-    key            = "expense-sg-dev"
+    key            = "expense-dev-cnd"
     region         = "us-east-1"
     dynamodb_table = "81s-locking"
-    use_lockfile = true
   }
 }
 
